ca4e764a362175007955d843ab78ad464633c4a5
Alberto Acquadro <96135308+albertoacquadro@users.noreply.github.com>
2026-01-19 10:22:26 +0100

    Update check_env.sh

--- scripts/check_env.sh
+++ scripts/check_env.sh
@@ -9,7 +9,7 @@ command -v vcftools >/dev/null && vcftools --version | head -n 1
 command -v fastp >/dev/null && fastp --version
 command -v fastqc >/dev/null && fastqc --version
 
-command -v bwa-mem2 >/dev/null && bwa-mem2 version
+command -v bwa >/dev/null && bwa 2>&1 | head -n 1
 
 command -v makeblastdb >/dev/null && makeblastdb -version | head -n 1
 command -v blastn >/dev/null && blastn -version | head -n 1
